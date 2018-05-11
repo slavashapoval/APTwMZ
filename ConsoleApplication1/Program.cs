c9019f7775aea73241e68f7d17ee6e938dd7adea
Viacheslav Shapov <viacheslavshapoval@gmail.com>
2018-05-11 14:12:17 +0200

    initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal money, percent;
+            int i;
+            const byte years = 5;
+
+            money = 5000.0m;
+            percent = 0.15m;
+
+            for (i = 1; i <= years; i++)
+            {
+                money *= 1 + percent;
+            }
+
+            Console.WriteLine("Całkowite przychody za {0} lat: {1} $$", years, money);
+            Console.ReadLine();
+        }
+    }
+}
